423b5dad3402e47f016681a323aaabec79be095d
KaYZeRT <37839506+KaYZeRT@users.noreply.github.com>
2020-03-23 15:59:33 +0100

    WIP: live plotting

--- temp.py
+++ temp.py
@@ -1,77 +1,107 @@
-from tkinter import *
-from random import randint
-
-# these two imports are important
+import tkinter as tk                # python 3
+from tkinter import font  as tkfont # python 3
 from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
 from matplotlib.figure import Figure
-import time
-import threading
+import matplotlib.animation as animation
 
-continuePlotting = False
+class SampleApp(tk.Tk):
 
+    def __init__(self, *args, **kwargs):
+        tk.Tk.__init__(self, *args, **kwargs)
 
-def change_state():
-    global continuePlotting
-    if continuePlotting == True:
-        continuePlotting = False
-    else:
-        continuePlotting = True
+        self.title_font = tkfont.Font(family='Helvetica', size=18, weight="bold", slant="italic")
 
+        # the container is where we'll stack a bunch of frames
+        # on top of each other, then the one we want visible
+        # will be raised above the others
+        container = tk.Frame(self)
+        container.pack(side="top", fill="both", expand=True)
+        container.grid_rowconfigure(0, weight=1)
+        container.grid_columnconfigure(0, weight=1)
 
-def data_points():
-    f = open("data.txt", "w")
-    for i in range(10):
-        f.write(str(randint(0, 10)) + '\n')
-    f.close()
+        self.frames = {}
+        for F in (StartPage, PageOne, PageTwo):
+            page_name = F.__name__
+            frame = F(parent=container, controller=self)
+            self.frames[page_name] = frame
 
-    f = open("data.txt", "r")
-    data = f.readlines()
-    f.close()
+            # put all of the pages in the same location;
+            # the one on the top of the stacking order
+            # will be the one that is visible.
+            frame.grid(row=0, column=0, sticky="nsew")
 
-    l = []
-    for i in range(len(data)):
-        l.append(int(data[i].rstrip("\n")))
-    return l
+        self.show_frame("StartPage")
 
-
-def app():
-    # initialise a window.
-    root = Tk()
-    root.config(background='white')
-    root.geometry("1000x700")
-
-    lab = Label(root, text="Live Plotting", bg='white').pack()
-
-    fig = Figure()
-    ax = fig.add_subplot(111)
-    ax.set_xlabel("X axis")
-    ax.set_ylabel("Y axis")
-    ax.grid(True)
-
-    graph = FigureCanvasTkAgg(fig, master=root)
-    graph.get_tk_widget().pack(side="top", fill='both', expand=True)
-    # graph.draw()
-
-    def plotter():
-        while continuePlotting:
-            ax.cla()
-            ax.grid()
-            dpts = data_points()
-            ax.plot(range(10), dpts, marker='o', color='orange')
-            # graph.draw()
-            print(dpts)
-            time.sleep(1)
-
-    def gui_handler():
-        change_state()
-        threading.Thread(target=plotter).start()
-        # plotter()
-
-    b = Button(root, text="Start/Stop", command=gui_handler, bg="red", fg="white")
-    b.pack()
-
-    root.mainloop()
+    def show_frame(self, page_name):
+        '''Show a frame for the given page name'''
+        frame = self.frames[page_name]
+        frame.tkraise()
 
 
-if __name__ == '__main__':
-    app()
+class StartPage(tk.Frame):
+
+    def __init__(self, parent, controller):
+        tk.Frame.__init__(self, parent)
+        self.controller = controller
+        label = tk.Label(self, text="This is the start page", font=controller.title_font)
+        label.pack(side="top", fill="x", pady=10)
+
+        button1 = tk.Button(self, text="Go to Page One",
+                            command=lambda: controller.show_frame("PageOne"))
+        button2 = tk.Button(self, text="Go to Page Two",
+                            command=lambda: controller.show_frame("PageTwo"))
+        button1.pack()
+        button2.pack()
+
+
+class PageOne(tk.Frame):
+
+    def __init__(self, parent, controller):
+        tk.Frame.__init__(self, parent)
+        self.controller = controller
+        label = tk.Label(self, text="This is page 1", font=controller.title_font)
+        label.pack(side="top", fill="x", pady=10)
+        button = tk.Button(self, text="Go to the start page",
+                           command=lambda: controller.show_frame("StartPage"))
+        button.pack()
+
+f = Figure(figsize=(5,5), dpi=100)
+a = f.add_subplot(111)
+
+def animate(i):
+    pullData = open("src/temp/sampleText.txt", "r").read()
+    dataList = pullData.split('\n')
+    xList = []
+    yList = []
+    for eachLine in dataList:
+        if len(eachLine) > 1:
+            x, y = eachLine.split(',')
+            xList.append(int(x))
+            yList.append(int(y))
+
+    a.clear()
+    a.plot(xList, yList)
+
+class PageTwo(tk.Frame):
+
+    def __init__(self, parent, controller):
+        tk.Frame.__init__(self, parent)
+        self.controller = controller
+        label = tk.Label(self, text="This is page 2", font=controller.title_font)
+        label.pack(side="top", fill="x", pady=10)
+        button = tk.Button(self, text="Go to the start page",
+                           command=lambda: controller.show_frame("StartPage"))
+        button.pack()
+
+        canvas = FigureCanvasTkAgg(f, self)
+        # canvas.show()
+        canvas.get_tk_widget().pack(side=tk.BOTTOM, fill=tk.BOTH, expand=True)
+        canvas.draw()
+
+        # ani = animation.FuncAnimation(f, animate, interval=1000)
+
+
+if __name__ == "__main__":
+    app = SampleApp()
+    ani = animation.FuncAnimation(f, animate, interval=1000)
+    app.mainloop()
